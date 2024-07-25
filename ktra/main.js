52df3ba3004ff917b63891f1c4361202687e40c6
DA-JSA03-ONL-HB <160859404+DA-JSA03-ONL-HB@users.noreply.github.com>
2024-07-25 20:06:05 +0700

    g

---
+++ ktra/main.js
@@ -0,0 +1,13 @@
+//câu 1
+// let m = [1,2,3,4,5,6,"hh","9",80,100]
+// m = m.filter(item => typeof item === 'number');
+// let msquared = m.map(item => math.pow(item,2));
+// console.log(m);
+// console.log(msquared);
+
+
+// //câu 2
+// let str = "high knowledge, high return";
+// let arr = str.split(' ').filter(item => item !== '');
+// arr = arr.map(item => item.toLocaleLowerCase());
+// console.log(arr);
